0b18f031f0e5e5a32a7aea078bd58490f0131de0
JobertSantos89 <48776579+JobertSantos89@users.noreply.github.com>
2024-08-26 03:51:04 -0300

    Add files via upload

---
+++ Provas Dev FullStack/Lógica de Programação com ChatGPT/prova_aula01.py
@@ -0,0 +1,6 @@
+base = float(input('Digite a medida da base do retângulo: '))
+altura = float(input('Digite a medida da altura do retângulo: '))
+
+area = base * altura
+
+print(f'A medida da área do retângulo é: {area:.2f}')
