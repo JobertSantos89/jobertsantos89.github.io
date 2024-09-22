d06abc3a9b2e0016cd7e642ba5b352f7aab08f6e
JobertSantos89 <48776579+JobertSantos89@users.noreply.github.com>
2024-09-22 15:58:58 -0300

    Add files via upload

---
+++ Lógica de Programação com ChatGPT/aula_05/script.py
@@ -0,0 +1,44 @@
+Sejam bem vindos a aula 5 do módulo de lógica de programação.
+
+Repositório da Turma
+https://github.com/Davi-Classes/PYTHON2024-DFS114/tree/main
+
+https://www.youtube.com/watch?v=iX6FJ3E2yBg&pp=ygUOUG9udG8gZW0gQ29tdW0%3D
+
+Assuntos que Vimos:
+
+- Estruturas Sequenciais
+- Estruturas Condicionais
+- Estruturas de Repetição (While)
+- Estruturas de Repetição (For)
+
+========================================================
+-> Contextos 
+
+Sobre Você:
+- "Meu nome é <nome>, atuo hoje como <profissão>, e sou estudante de programação. Atualmente estou estudando logica de programação em Python (Input, Print, If, While e For)"
+
+Persona da IA:
+- "Quero que a <IA> me responda agindo como um Desenvolvedor Sênior com foco em python e com experiência em ministrar aulas."
+
+-> Prompts
+- "Olá <IA>, agindo como um desenvolvedor experiênte gere para mim 4 exercicios sobre <assunto>. Utilize sua imaginação e seja criativo!"
+
+- "Olá ChatGPT, crie para mim <Quantidade de Exercicios> exercicios de <Assunto> (<Tópico>) onde você mostrará as questões como uma Prova! Funcionará da seguinte forma:
+
+1. Você manda a questão
+2. Eu respondo a questão
+3. Você responde se a questão está correta, caso esteja errada, mostre a correção 
+4. Mostre a proxima questão e repita o ciclo até ter respondido <Quantidade de Exercicios> exercicios."
+
+- "Olá ChatGPT, estou com dificuldade para resolver o exercicio a seguir: 
+"""
+Faça um programa que peça 10 números inteiros, calcule e mostre a quantidade de números pares e a quantidade de números impares.
+"""
+Me dê uma dica de qual linha de raciocinio devo utilizar para solucionar (SEM ME ENTREGAR A RESPOSTA)"
+
+- "Olá Gepeto, detalhe e explique para mim linha por linha o codigo em python:
+"""
+<CODIGO>
+""" 
+"
