4b7a54d07eeb12a437bf45e789394a874f97a6ea
JobertSantos89 <48776579+JobertSantos89@users.noreply.github.com>
2024-10-20 16:45:18 -0300

    Delete Python IA/Aula 01 Listas e Tuplas directory

--- Python IA/Aula 01 Listas e Tuplas/ex01.py
+++
@@ -1,9 +0,0 @@
-#Ex01. Crie uma lista preenchida com 5 valores:
-
-#-> Mostre na tela o elemento na posição 2
-#-> Mostre na tela o 2º elemento da lista
-
-lista = ['Davi', 'Fernanda', 'Loki', 'Café', 'Margot']
-
-print(lista[2]) # Posição 2 = 3º Elemento
-print(lista[1]) # Posição 1 = 2º Elemento
--- Python IA/Aula 01 Listas e Tuplas/ex02.py
+++
@@ -1,7 +0,0 @@
-nomes = []
-
-for n in range(3):
-    nome = input(f'Digite o {n + 1}º nome: ')
-    nomes.append(nome)
-
-print(nomes)
