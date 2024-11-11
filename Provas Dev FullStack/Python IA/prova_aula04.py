0d8af34cbbfa8dcf3c365a31882f1810008685f9
JobertSantos89 <48776579+JobertSantos89@users.noreply.github.com>
2024-11-11 04:08:56 -0300

    Add files via upload

---
+++ Provas Dev FullStack/Python IA/prova_aula04.py
@@ -0,0 +1,8 @@
+def media(num1, num2, num3):
+    # Calcula a média aritmética dos três números recebidos como argumentos
+    media_aritmetica = (num1 + num2 + num3) / 3
+    return media_aritmetica
+
+# Chamando a função com argumentos
+resultado = media(50, 15, 10)
+print("A média é:", resultado)
